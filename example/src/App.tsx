42f56c444a6b912bc16485ba4cbf24d0f94ab17c
mlapointe <mark@cloudbsd.org>
2021-03-08 23:41:23 -0500

    Init

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'cloudbsd-react-admin'
+import 'cloudbsd-react-admin/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
